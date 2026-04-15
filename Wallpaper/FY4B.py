77d4ede6b1bf5bccadbd4fab7586a14c62cc3e90
storm-1614 <storm1614@outlook.com>
2026-04-15 22:19:38 +0800

    fun: raw image download error retry

--- Wallpaper/FY4B.py
+++ Wallpaper/FY4B.py
@@ -11,23 +11,45 @@ downloadPath = os.path.join(os.environ["HOME"] + "/.cache/fy4b/")
 Image.MAX_IMAGE_PIXELS = 300000000  # 设置最大图片尺寸
 
 
-## 检查目录是否创建，如果没有则创建
 def checkDir(downloadPath):
+    """
+    检查目录是否创建，如果没有则创建
+    """
     mkdir = lambda x: os.makedirs(x) if not os.path.exists(x) else True
     mkdir(downloadPath)
 
 
-# 下载壁纸
 def downloadWallpaper():
-    checkDir(downloadPath)  ## 检查目录创建
+    """
+    下载壁纸
+    调用 requests 库，下载 nsmc 的 fy4b 真彩云图
+    """
+    checkDir(downloadPath)  
     picture_url = "http://img.nsmc.org.cn/CLOUDIMAGE/FY4B/AGRI/GCLR/FY4B_DISK_GCLR.JPG"  ## 下载 FY4B 链接
-    res = requests.get(picture_url)  ### 创建一个 res 对象内容：下载图片
-    global downloadFile
-    downloadFile = os.path.join(downloadPath+time.strftime('%Y-%m-%d_%H:%M')+"-img.jpg")
-    #downloadFile = os.path.join(downloadPath + "img.jpg")
-    ## 写入
-    with open(downloadFile, "wb") as f:
-        f.write(res.content)
+    retry_count = 0
+    max_retry = 5
+    while retry_count < max_retry:
+        try:
+            print("下载中")
+            res = requests.get(picture_url)  ### 创建一个 res 对象内容：下载图片
+            print("下载成功")
+            global downloadFile
+            downloadFile = os.path.join(
+                downloadPath + time.strftime("%Y-%m-%d_%H:%M") + "-img.jpg"
+            )  ## 写入
+            with open(downloadFile, "wb") as f:
+                f.write(res.content)
+            break
+        except requests.exceptions.ConnectTimeout:
+            retry_count += 1
+            print(f"下载超时，正在进行第 {retry_count} 次重试...")
+            if retry_count >= max_retry:
+                print("错误次数超过 {max_retry} 次，下载失败")
+        except requests.exceptions.ConnectionError:
+            retry_count += 1
+            print(f"连接错误，正在进行第 {retry_count} 次重试...")
+            if retry_count >= max_retry:
+                print("错误次数超过 {max_retry} 次，下载失败")
 
 
 ## 剪裁图片使之与屏幕相符
@@ -52,10 +74,9 @@ def update():
     """
     壁纸更新函数
     """
-    time.sleep(1);
+    time.sleep(1)
     print("=================")
     print(f"[{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] 执行")
-    print("下载中")
     downloadWallpaper()
     print("下载成功")
     cropWallpaper()
@@ -63,10 +84,17 @@ def update():
     setWallpaper()
     print("壁纸设置成功")
 
+
 if __name__ == "__main__":
-    scheduler = BackgroundScheduler() # 创建一个后台调度器
-    scheduler.add_job(update, 'interval', minutes = 15, id='update_wallpaper', next_run_time=datetime.datetime.now()) # 添加任务
-    scheduler.start() # 开启调度器
+    scheduler = BackgroundScheduler()  # 创建一个后台调度器
+    scheduler.add_job(
+        update,
+        "interval",
+        minutes=15,
+        id="update_wallpaper",
+        next_run_time=datetime.datetime.now(),
+    )  # 添加任务
+    scheduler.start()  # 开启调度器
     print(f"[{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] 调度器已启动")
     try:
         while True:
